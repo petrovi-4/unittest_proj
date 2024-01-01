f09551f23c00aad9d2d5208adc80798d397b043c
petrovi4 <40.10.444@gmail.com>
2024-01-01 16:58:58 +0300

    test cov 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,10 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_slice_empty_list(self):
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
+
+    def test_slice_negative_end(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], end=-1), [1, 2, 3])
+
